82c88e04b1219d17690a0d26f1cb96ed46aaccef
kangjaee <163740570+kangjaee@users.noreply.github.com>
2024-03-27 12:24:44 +0900

    Create BreakDemo.java

---
+++ 폴더/BreakDemo.java
@@ -0,0 +1,14 @@
+package sec02;
+
+public class BreakDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1, j=5;
+		
+		while (true) {
+			System.out.print(i++);
+			if(i>=j)
+				break;
+		}
+	}
+}
