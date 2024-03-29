22977ca595b33bdffa3a0de49e2e00ae7fcd2a20
kangjaee <163740570+kangjaee@users.noreply.github.com>
2024-03-29 13:03:12 +0900

    Create 도전과제1.java

---
+++ 4주차/도전과제1.java
@@ -0,0 +1,16 @@
+class Printer {
+	int numOfPapers = 0;
+
+	public void print(int amount) {
+		numOfPapers -= amount;
+	}
+}
+
+public class PrinterTest {
+	public static void main(String[] args) {
+		Printer p = new Printer();
+		p.numOfPapers = 100;
+		p.print(70);
+		System.out.println(p.numOfPapers);
+	}
+}
